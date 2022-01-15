9a9afb01694ed96fc56b26b99e22ba9013391dac
nascott <nascott94@outlook.com>
2022-01-15 16:04:52 -0700

    basic layout for footer

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,5 +1,17 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ReviewsStyles = styled.div`
+  overflow-x: hidden;
+  padding: 10rem 0;
+  text-align: center;
+
+  .testimonial_info {
+  }
+`;
+
 const Reviews = () => {
-  return <h1>Hello</h1>;
+  return <ReviewsStyles>Hello</ReviewsStyles>;
 };
 
 export default Reviews;
