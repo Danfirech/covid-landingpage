ffe914dda80b5cdfc7e9acb60791c0c1ef868a37
nascott <nascott94@outlook.com>
2022-01-15 15:08:17 -0700

    start of footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 10%;
+  background-color: #96a480;
+  color: lightgray;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    padding: 10px;
+  }
+`;
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+`;
+
+const ListItem = styled.li`
+  margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    margin-right: 10px;
+    font-size: 14px;
+  }
+`;
+const Copyright = styled.span`
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
+`;
+
 const Footer = () => {
-  return <h1>Hello</h1>;
+  return (
+    <Container>
+      <Wrapper>
+        <List>
+          <ListItem>Guide</ListItem>
+          <ListItem>Support</ListItem>
+          <ListItem>API</ListItem>
+          <ListItem>Community</ListItem>
+        </List>
+        <Copyright>NAScottDesign Ⓒ</Copyright>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Footer;
