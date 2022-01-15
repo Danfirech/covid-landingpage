c17258edecc4e9230bd2805ef658d0ceaf4a1918
Dan Possehl <danielbpossehl@gmail.com>
2022-01-15 15:04:08 -0700

    added styled components folder in document dot js file

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,39 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+`;
+
+const StyledLink = styled.a`
+  padding: 0rem 2rem;
+`;
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Dano</h1>
-      <h2>indigo</h2>
-    </div>
+    <Nav>
+      <div>
+        <Link href="/" passHref>
+          <StyledLink>NXT</StyledLink>
+        </Link>
+      </div>
+      <div>
+        <Link href="/" passHref>
+          <StyledLink>Home</StyledLink>
+        </Link>
+        <Link href="/about" passHref>
+          <StyledLink>About</StyledLink>
+        </Link>
+        <Link href="/contact" passHref>
+          <StyledLink>Contact</StyledLink>
+        </Link>
+      </div>
+    </Nav>
   );
 };
 
