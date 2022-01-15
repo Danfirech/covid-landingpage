7c71b23528cf16b16dff42b5b60bad58c5ddda47
Dan Possehl <danielbpossehl@gmail.com>
2022-01-15 15:37:24 -0700

    Call to action set up

--- components/About.js
+++ components/About.js
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+
+const AboutContainerTop = styled.div`
+  height: 300px;
+  width: 100vw;
+  background-color: blue;
+`;
+
+const AboutContainerBottom = styled.div`
+  height: 300px;
+  width: 100vw;
+  background-color: green;
+`;
+
+const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 130px;
+  background-color: black;
+  color: white;
+`;
+
 const About = () => {
-  return <h1>BIG BOI</h1>;
+  return (
+    <>
+      <AboutContainerTop />
+      <AboutContainerBottom />
+    </>
+  );
 };
 
 export default About;
