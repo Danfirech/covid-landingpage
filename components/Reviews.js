5598b98b51ca4235b59b11a83a24525156f41bdc
nascott <nascott94@outlook.com>
2022-01-15 17:55:24 -0700

    added assets folder and working on reviews components

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,17 +1,151 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { MdArrowForward, MdArrowBack } from 'react-icons/md';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import testimonials from '../assets/data/testimonials';
 
 const ReviewsStyles = styled.div`
+  height: 10%;
   overflow-x: hidden;
   padding: 10rem 0;
   text-align: center;
+  background-color: #ffb703;
 
-  .testimonial_info {
+  .review__wrapper {
+    position: relative;
+    max-width: 700px;
+    margin: 0 auto;
+  }
+  .review__info {
+    width: 100%;
+    height: fit-content;
+    padding: 3rem;
+    background-color: var(--deep-dark);
+    border-radius: 12px;
+    margin-top: 5rem;
+  }
+  .review__desc {
+    .para {
+      text-align: center;
+    }
+  }
+  .review__name {
+    margin-top: 4rem;
+    font-family: 'Montserrat Bold';
+    font-size: 2.2rem;
+  }
+  .review__title {
+    font-size: 1.6rem;
+    margin-top: 0.3rem;
+  }
+  .arrows {
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      width: 30px;
+      pointer-events: none;
+    }
+    .next,
+    .prev {
+      margin: 0 0.5rem;
+      width: fit-content;
+      background-color: var(--deep-dark);
+      padding: 0.5rem 2rem;
+      border-radius: 8px;
+      cursor: pointer;
+    }
+  }
+  .fade-enter {
+    opacity: 0;
+    transform: scale(0.96);
+    z-index: 1;
+  }
+  .fade-enter-active {
+    opacity: 1;
+    transform: scale(1);
+    transition: 250ms ease-in;
+    transition-property: transform, opacity;
+    z-index: 1;
+  }
+  .fade-exit {
+    transform: scale(1);
+    opacity: 1;
+  }
+  .fade-exit-active {
+    opacity: 0;
+    transform: scale(0.96);
+    transition: 200ms ease-in;
+    transition-property: transform, opacity;
   }
 `;
 
 const Reviews = () => {
-  return <ReviewsStyles>Hello</ReviewsStyles>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const activeSlide = testimonials[activeIndex];
+
+  function handleNext() {
+    if (activeIndex >= testimonials.length - 1) {
+      setActiveIndex(0);
+    } else {
+      setActiveIndex((oldIndex) => oldIndex + 1);
+    }
+  }
+  function handlePrev() {
+    if (activeIndex === 0) {
+      setActiveIndex(testimonials.length - 1);
+    } else {
+      setActiveIndex((oldIndex) => oldIndex - 1);
+    }
+  }
+
+  return (
+    <ReviewsStyles>
+      <div className="container">
+        <h1
+          subheading="see what our clients say about us"
+          heading="Testimonials"
+        ></h1>
+        <div className="testimonial__wrapper">
+          <SwitchTransition component={null}>
+            <CSSTransition key={activeSlide.id} timeout={300} classNames="fade">
+              <div className="testimonial__info">
+                <div className="testimonial__desc">
+                  <div>{activeSlide.desc}</div>
+                </div>
+                <h2 className="testimonial__name">{activeSlide.name}</h2>
+                <p className="testimonial__title">
+                  {activeSlide.title}, <br /> {activeSlide.org}
+                </p>
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
+
+        <div className="arrows">
+          <div
+            className="prev"
+            onClick={handlePrev}
+            role="button"
+            tabIndex={0}
+            onKeyDown={handlePrev}
+          >
+            <MdArrowBack />
+          </div>
+          <div
+            className="next"
+            onClick={handleNext}
+            role="button"
+            tabIndex={0}
+            onKeyDown={handleNext}
+          >
+            <MdArrowForward />
+          </div>
+        </div>
+      </div>
+    </ReviewsStyles>
+  );
 };
 
 export default Reviews;
