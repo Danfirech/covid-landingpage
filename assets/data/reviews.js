fd055fe53b4860761845c6c0bd5ccd411406990d
nascott <nascott94@outlook.com>
2022-01-18 08:31:13 -0700

    changed reviews

--- assets/data/reviews.js
+++ assets/data/reviews.js
@@ -4,21 +4,21 @@ const reviews = [
     name: 'Joji Scott',
     title: 'CEO',
     org: 'WEB CIFAR',
-    desc: 'covid is bad bad not good covid is bad bad not good covid is bad bad not good covid is bad bad not good',
+    desc: 'Professional service that delivered a quick COVID test. I would highly recommend. Effecient response when I could not get testing elsewhere for several days.',
   },
   {
     id: 2,
     name: 'Clay Beezhold',
     title: 'Co-founder',
     org: 'Kreetive',
-    desc: 'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+    desc: 'Really amazing communication skills. They do really high-quality work.',
   },
   {
     id: 3,
     name: 'Allie Prins',
     title: 'Marketing Head',
     org: 'Eleven Dawn',
-    desc: 'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+    desc: 'Excellent service! The test swab taker was a total professional.',
   },
 ];
 export default reviews;
